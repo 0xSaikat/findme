5c0b562212c1c0d2efac34f11ba8304391417cfa
Sakil Hasan Saikat <72136146+0xSaikat@users.noreply.github.com>
2025-01-14 12:56:59 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
-            "findme=findme:main",  
+            "findme=findme.findme:main",  
         ],
     },
 )
