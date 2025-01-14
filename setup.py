85e7096ae9448856ea50626553d4b2608438ed66
Sakil Hasan Saikat <72136146+0xSaikat@users.noreply.github.com>
2025-01-14 20:11:53 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-# Load long description from README.md safely
+
 try:
     with open("README.md", "r", encoding="utf-8") as fh:
         long_description = fh.read()
