848571fe5522d6d18b412b28146205a894dee327
Sakil Hasan Saikat <72136146+0xSaikat@users.noreply.github.com>
2025-01-14 16:18:53 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="findme",
+    name="findme-hackbit.org",
     version="1.0.4",
     packages=find_packages(),
     include_package_data=True,
